6311ee32c73d93d999156727052e758a0a9f6ba2
evply <evply@sina.com>
2014-12-04 08:53:01 +0800

    init

--- config/evply-package.el
+++ config/evply-package.el
@@ -4,8 +4,8 @@
 ;;elpa
 (require 'package)
 
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 (defun require-package (package)
   (if (not (package-installed-p package))
--- config/evply-web.el
+++ config/evply-web.el
@@ -5,6 +5,11 @@
 
 (require-package 'js2-mode)
 
+(require-package 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
+
 ;;package from melpa
 (require-package 'ac-js2)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
